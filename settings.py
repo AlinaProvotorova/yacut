0a494c7ebe2e636c067204118ccc81539bcd535a
alinamalinapro <provotorovaalin@yandex.ru>
2023-02-06 22:18:49 +0500

    Проект YaCut

--- settings.py
+++ settings.py
@@ -14,4 +14,3 @@ VALID_CHARACTERS = ascii_letters + digits
 PATTERN_VALID_CHARACTERS = r'^[A-Za-z0-9]+$'
 PATTERN_VALID_URL = r'(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$'
 MAX_COUNT_CREATE_SHORT = len(VALID_CHARACTERS) ** MAX_SIZE_SHORT - 1
-
